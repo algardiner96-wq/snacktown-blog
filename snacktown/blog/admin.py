0cfd8926e5cfb8f2c0934a75288c55da7ba9e7bf
AL Gardiner <algardiner96@gmail.com>
2025-12-08 15:24:25 +0000

    Restarting due to copilot delete logs

--- snacktown/blog/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from .models import MenuItem, Review, BlogPost
-# Register your models here.
-
-admin.site.register(MenuItem)
-admin.site.register(Review)
-admin.site.register(BlogPost)
