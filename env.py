1b25b5145e16b94382f065decb7db7ba96b786c4
AL Gardiner <algardiner96@gmail.com>
2025-12-08 18:20:04 +0000

    Change secret key

---
+++ env.py
@@ -0,0 +1,9 @@
+import os 
+
+
+
+os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_Fuh4awEg9jdZ@ep-wild-flower-aghjoqwe.c-2.eu-central-1.aws.neon.tech/jab_goal_roman_629175"   
+
+
+
+os.environ["SECRET_KEY"] = "f5ysteax9y(l(t#wf9b!7ivomw3-^u7p^036-wm&ka&5m574nd"
