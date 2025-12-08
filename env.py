74727a558219dd7e0672a063a3c3c10de5a7eab3
AL Gardiner <algardiner96@gmail.com>
2025-12-08 18:08:21 +0000

    Remove .venv and env.py from tracking

--- env.py
+++
@@ -1,9 +0,0 @@
-import os 
-
-
-os.environ["DATABASE_URL"] = "postgresql://neondb_owner:npg_Fuh4awEg9jdZ@ep-wild-flower-aghjoqwe.c-2.eu-central-1.aws.neon.tech/jab_goal_roman_629175"
-
-
-
-os.environ["SECRET_KEY"] = "django-insecure-m3b6jg$)hr0)(rp_%1-cp00g7%bm*&q6hp0&1nt%+2g@4kg-jh"
-
